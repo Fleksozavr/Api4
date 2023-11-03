9ececd8048e844ab71f1a816e1a272f47d665ecf
Fleksozavr <131782632+Fleksozavr@users.noreply.github.com>
2023-11-03 16:34:32 +0500

    Починил код

--- fetch_epic.py
+++ fetch_epic.py
@@ -1,34 +1,37 @@
 import os
 import requests
-from urllib.parse import urlparse
+from urllib.parse import urlencode, urlparse
 from save_tools import save_picture
 import argparse
+from dotenv import load_dotenv
 
 
 def get_nasa_images(api_key):
-    response = requests.get(f'https://api.nasa.gov/EPIC/api/natural/images?{urllib.parse.urlencode(params)}')
+    params = {'api_key': api_key}
+    response = requests.get(f'https://api.nasa.gov/EPIC/api/natural/images?{urlencode(params)}')
     response.raise_for_status()
     return response.json()
 
 
-def main(args):
+def main():
     load_dotenv()
-    params = {'api_key': api_key}
-    epic_api_key = os.getenv('EPIC_API_KEY')
-    urlbase = 'https://api.nasa.gov/EPIC/archive/natural/'
-    folder = os.path.join(os.path.expanduser('~'), 'Desktop', 'Api4', 'Images')
-    filename = 'epic'
+    api_key = os.getenv('NASA_API_KEY')
+    if not api_key:
+        print('API ключ не найден')
+        return
+
     parser = argparse.ArgumentParser(description='Скачивание фотографий с NASA API')
-    parser.add_argument('-k', '--api_key', type=str, help='API ключ для доступа к NASA API', required=True)
     parser.add_argument('-f', '--folder', type=str, help='Путь к папке для сохранения фотографий', required=True)
     args = parser.parse_args()
-    nasa_images = get_nasa_images(args.api_key)
+
+    nasa_images = get_nasa_images(api_key)
+    url_base = 'https://api.nasa.gov/EPIC/archive/natural/'
 
     for nasa_image in nasa_images:
         name_epic = nasa_image['image']
         date_epic = nasa_image['date']
         full_name = os.path.join(args.folder, f"{name_epic}.png")
-        url_photo = f'{url_base}/{epic_date[:4]}/{date_epic[5:7]}/{date_epic[8:10]}/png/{date_epic}.png?{urllib.parse.urlencode(params)}'
+        url_photo = f'{url_base}/{date_epic[:4]}/{date_epic[5:7]}/{date_epic[8:10]}/png/{name_epic}.png?api_key={api_key}'
         save_picture(args.folder, url_photo, full_name)
 
 

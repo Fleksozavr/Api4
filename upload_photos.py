d0659e34781ae47f23b6fa1b9485d766983f8716
Fleksozavr <131782632+Fleksozavr@users.noreply.github.com>
2023-11-06 01:21:16 +0500

    Починил код

--- upload_photos.py
+++ upload_photos.py
@@ -9,7 +9,7 @@ def main():
     load_dotenv()
     telegram_token = os.getenv('TELEGRAM_TOKEN')
     telegram_channel_id = os.getenv('TELEGRAM_CHANNEL_ID')
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = telegram.Bot(token=telegram_token)
 
     parser = argparse.ArgumentParser(description='Отправка случайных фотографий в чат Telegram')
     parser.add_argument('--time',
@@ -20,9 +20,17 @@ def main():
                         type=str,
                         default='folder',
                         help='Путь к папке, содержащей фотографии для отправки')
+    parser.add_argument('--telegram_token',
+                        type=str,
+                        help='Токен бота Telegram')
+    parser.add_argument('--telegram_channel_id',
+                        type=str,
+                        help='ID канала Telegram')
     args = parser.parse_args()
     folder = args.folder
     periodicity = args.time
+    telegram_token = args.telegram_token or telegram_token
+    telegram_channel_id = args.telegram_channel_id or telegram_channel_id
 
     while True:
         all_files = []
@@ -35,8 +43,8 @@ def main():
 
         random_file = random.choice(all_files)
         try:
-            with open(random_file, 'rb') as file:
-                bot.send_document(chat_id=TELEGRAM_CHANNEL_ID, document=file)
+            with open(random_file, 'rb') as file_obj:
+                bot.send_document(chat_id=telegram_channel_id, document=file_obj)
             print(f'Отправлена фотография: {random_file}')
         except telegram.error.TelegramError as e:
             print(f'Произошла ошибка при отправке: {e}')
